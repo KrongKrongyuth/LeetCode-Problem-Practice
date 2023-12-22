480c42c1d14ec97cf6889d3eef20707cbab5f532
TON_KKrongyuth <ton.kkrongyuth@gmail.com>
2023-12-22 22:12:29 +0700

    commit Longest_Substring_Without_Repeating_Characters.py_Ver4

--- Longest_Substring_Without_Repeating_Characters.py
+++ Longest_Substring_Without_Repeating_Characters.py
@@ -1,17 +1,18 @@
 class Solution(object):
-    def lengthOfLongestSubstring(self, s):
+    def lengthOfLongestSubstring(self, s) :
         if s.isalpha() and len(s) > 1 :
-            word_dict = {}
+            max_word = 0
             for i in range(len(s)) :
                 current_word = ""
                 for j in range(len(s[i:])) :
                     starter = s[i+j]
                     if starter not in current_word : 
                         current_word += starter
-                        word_dict[current_word] = len(current_word)
+                        len_current = len(current_word)
+                        if len_current >= max_word : max_word = len_current
                     else :
                         break
-            return max(word_dict.values())
+            return max_word
         else : return len(set(s))
 
 s = 'pwwkew'
